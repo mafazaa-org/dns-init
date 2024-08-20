92a478e9821029b54a14ce575fd5d4967bfa3fdc
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-08-21 02:42:37 +0300

    setting the dns-admin user before env variables

--- init.sh
+++ init.sh
@@ -11,6 +11,29 @@ echo
 echo installing python and pip
 sudo apt install python3 python3-pip -y
 
+## setup dns-admin user
+
+home=/home/dns-admin
+
+sudo useradd dns-admin --no-user-group --shell /bin/bash --home-dir $home
+sudo mkdir $home
+
+echo please add the user dns-admin to the sudoers file
+
+read
+
+sudo visudo
+
+sudo cp ~/.ssh $home/.ssh
+
+sudo chown -hR dns-admin $home/.ssh
+
+sudo git clone $DNS_REPOS_ROOT/$DNS_INIT $home/dns-init
+
+echo the next time, login with dns-admin so the script can delete the first user
+
+read
+
 # setting repos environment variables
 
 export DNS_REPOS_ROOT=https://github.com/mafazaa-org
@@ -19,44 +42,23 @@ export DNS_SERVER=dns-server
 export DNS_CHECK=dns-check
 export DNS_IP_CONFIRM=dns-ip-confirm
 
-echo export DNS_REPOS_ROOT=$DNS_REPOS_ROOT >> ~/.bashrc
-echo export DNS_INIT=$DNS_INIT >> ~/.bashrc
-echo export DNS_SERVER=$DNS_SERVER >> ~/.bashrc
-echo export DNS_CHECK=$DNS_CHECK >> ~/.bashrc
-echo export DNS_IP_CONFIRM=$DNS_IP_CONFIRM >> ~/.bashrc
+echo export DNS_REPOS_ROOT=$DNS_REPOS_ROOT >> $home/.bashrc
+echo export DNS_INIT=$DNS_INIT >> $home/.bashrc
+echo export DNS_SERVER=$DNS_SERVER >> $home/.bashrc
+echo export DNS_CHECK=$DNS_CHECK >> $home/.bashrc
+echo export DNS_IP_CONFIRM=$DNS_IP_CONFIRM >> $home/.bashrc
 
-source ~/.bashrc
+source $home/.bashrc
 
 crontab_file=/var/spool/cron/crontabs/dns-admin
 
-echo DNS_REPOS_ROOT=$DNS_REPOS_ROOT >> $crontab_file
-echo DNS_INIT=$DNS_INIT >> $crontab_file
-echo DNS_SERVER=$DNS_SERVER >> $crontab_file
-echo DNS_CHECK=$DNS_CHECK >> $crontab_file
-echo DNS_IP_CONFIRM=$DNS_IP_CONFIRM >> $crontab_file
+sudo echo DNS_REPOS_ROOT=$DNS_REPOS_ROOT >> $crontab_file
+sudo echo DNS_INIT=$DNS_INIT >> $crontab_file
+sudo echo DNS_SERVER=$DNS_SERVER >> $crontab_file
+sudo echo DNS_CHECK=$DNS_CHECK >> $crontab_file
+sudo echo DNS_IP_CONFIRM=$DNS_IP_CONFIRM >> $crontab_file
 
-echo 0 0 0 0-30/10 * rm /home/dns-admin/.dns-logs
-
-## setup dns-admin user
-
-sudo useradd dns-admin --no-user-group --shell /bin/bash --home-dir /home/dns-admin
-sudo mkdir /home/dns-admin
-
-echo please add the user dns-admin to the sudoers file
-
-read
-
-sudo visudo
-
-sudo cp ~/.ssh /home/dns-admin/.ssh
-
-sudo chown -hR dns-admin /home/dns-admin/.ssh
-
-sudo git clone $DNS_REPOS_ROOT/$DNS_INIT /home/dns-admin/dns-init
-
-echo the next time, login with dns-admin so the script can delete the first user
-
-read
+echo 0 0 0 0-30/10 * rm home/dns-admin/.dns-logs
 
 ## freeing port 53
 
